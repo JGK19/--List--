1a40d6bca7947447a4cd174696ee898fd7e58e3c
JGK19 <106597685+JGK19@users.noreply.github.com>
2023-09-23 21:36:40 -0300

    Create List.h

---
+++ List.h
@@ -0,0 +1,23 @@
+// adicionar elementos na lista
+void append(List *list, int var);
+void putFirst(List *list, int var); // TODO
+
+// remover elementos da lista
+void pop(List *list);
+void popIndex(List *list, int index);
+void removeFirst(List *list, int index); // TODO
+void removeElement(List *list, int var); // TODO
+
+// Ler elementos da Lista
+int get(List *list, int index);
+void printList(List *list);
+
+// Escrever em elementos da lista
+void *getNode(List *list, int index);
+
+// list tools
+void freeList(List *list);
+
+// Ordenar lista
+void bubblesort(List *list);
+void troca(Node *x, Node *y);
